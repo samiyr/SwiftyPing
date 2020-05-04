f7ac9648feddf264bd5013900e2ac87f99aba5b6
samiyr <7385107+samiyr@users.noreply.github.com>
2020-05-04 09:11:12 +0300

    Response validation
    Responses are now validated, with the exception that the checksums are not compared as this doesn't work at the moment. Code was also cleaned up and some bugs were fixed.

--- SwiftyPingTest/ViewController.swift
+++ SwiftyPingTest/ViewController.swift
@@ -26,20 +26,25 @@ class ViewController: UIViewController {
     var ping: SwiftyPing?
     func startPinging() {
         do {
-            ping = try SwiftyPing(host: "1.1.1.1", configuration: PingConfiguration(interval: 1.0), queue: DispatchQueue.global())
+            ping = try SwiftyPing(host: "1.1.1.1", configuration: PingConfiguration(interval: 1.0, with: 1), queue: DispatchQueue.global())
         } catch {
             textView.text = error.localizedDescription
         }
-        ping?.observer = { (ping, response) in
+        ping?.observer = { (response) in
             DispatchQueue.main.async {
                 var message = "\(response.duration * 1000) ms"
                 if let error = response.error {
-                    message = error.localizedDescription
+                    if error == .responseTimeout {
+                        message = "Timeout \(message)"
+                    } else {
+                        message = error.localizedDescription
+                    }
                 }
                 self.textView.text.append(contentsOf: "\nPing #\(response.sequenceNumber): \(message)")
                 self.textView.scrollRangeToVisible(NSRange(location: self.textView.text.count - 1, length: 1))
             }
         }
+//        ping?.targetCount = 1
         ping?.startPinging()
     }
 
